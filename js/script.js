f3cb8da46aeb8a50d4f850722259ced92eed86c8
KUOPING LEE <blu_blac@hotmail.com>
2016-10-03 13:33:09 +0800

    version 1

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  // alert('DOM is ready')
+  var canvas = document.getElementById('canvas')
+  var ctx = canvas.getContext('2d')
+  var background = new Image()
+  background.src = 'assets/pitch.png'
+  ctx.drawImage(background, 0, 0, 700, 500)
+
+  // var getBall = document.getElementById('ball')
+  var ball = new Image()
+  ball.src = 'assets/soccerball.png'
+
+  var $startBtn = $('#startBtn')
+  var score
+  var countDown = 60
+  var timerID
+
+// innitiate event listener for start button to start game
+  $startBtn.on('click', startGame)
+  // invoke random balls to appear within the canvas, with random duration
+  function startGame () {
+    // var time = Math.random() * 4000
+    countDown -= 1
+    timerID = window.setTimeout(ballGenerator, Math.random() * 1000)
+    function ballGenerator () {
+      // function to generate ball to appear randomly
+      // function getpos(min, max) {
+      //     return Math.random() * (max - min) + min;
+      // }
+      // ymin = canvas.offsetLeft
+      // ymax = ymin + canvas.offsetWidth
+      // xmin = canvas.offsetTop
+      // xmax = xmin + canvas.offsetHeight
+      ctx.drawImage(ball, Math.random() * 650, Math.random() * 450, 50, 50)
+      // console.log(startGame())
+    }
+  };
+  startGame()
+// once ball appear, event listener for on click, ball disappear
+// each successful click, scores 1 point
+    // ball.on('click', function)
+        // ball.remove()
+        // score ++;
+
+// to remove ball randomly between 0.5 sec and 2secs
+    // if (Math.random()*2000 > 0.5) {
+        // window.clearTimeout(timerID)
+    // }
+
+// game ends at end of countdown timer
+})
